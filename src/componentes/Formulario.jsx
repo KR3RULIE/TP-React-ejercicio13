b6fbf9248a8aa8640db2263f75ca969a949e6e0b
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-29 15:06:23 -0300

    hice el mapeado del clima y tambien el form con los 2 campos, hice la funcion que recime el dato de la api y agregue algunos estilos

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,9 +1,63 @@
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import ListaClima from "./ListaClima";
+
 const Formulario = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [ciudad, setCiudad] = useState("");
+  const [pais, setPais] = useState("");
+  const [clima, setClima] = useState({});
+  const API_KEY = "13d8d14a31e3e44c55a7993cbb769ce4";
+
+  const consultarClima = async () => {
+    if (ciudad.trim() === "" || pais.trim() === "") {
+      alert("Por favor, ingresa ciudad y país");
+      return;
+    }
+    try {
+      const respuesta = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${API_KEY}&units=metric&lang=es`
+      );
+      if (respuesta.status === 200) {
+        const datos = await respuesta.json();
+        console.log(datos);
+        setClima(datos);
+      }
+    } catch (error) {
+      alert(error.message);
+      setClima({});
+    }
+  };
+
+  return (
+    <>
+      <section className="container bg-primary my-5 p-3 rounded">
+        <Form>
+          <Form.Group>
+            <Form.Label className="fw-bold">Escribe el pais:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="AR = Argentina"
+              value={pais}
+              onChange={(e) => setPais(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="my-3">
+            <Form.Label className="fw-bold">Escribe la ciudad:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Buenos Aires"
+              value={ciudad}
+              onChange={(e) => setCiudad(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button variant="success" className="mt-3" onClick={consultarClima}>
+            Consultar
+          </Button>
+        </Form>
+      </section>
+      <ListaClima clima={clima}></ListaClima>
+    </>
+  );
 };
 
 export default Formulario;
---
+++ src/componentes/ListaClima.jsx
@@ -0,0 +1,26 @@
+const ListaClima = ({ clima }) => {
+  if (clima === undefined || clima.main === undefined) {
+    return <p className="text-center">No se encontraron datos aún</p>;
+  }
+  return (
+    <section className="text-center mt-3">
+      <h3>
+        Clima en {clima.name}, {clima.sys.country}
+      </h3>
+      <p>🌡️ Temperatura: {clima.main.temp} °C</p>
+      <p>
+        📉 Mín: {clima.main.temp_min} °C | 📈 Máx: {clima.main.temp_max} °C
+      </p>
+      <p>💧 Humedad: {clima.main.humidity}%</p>
+      <p>
+        🌤️ Condición:{" "}
+        {clima.weather && clima.weather[0]
+          ? clima.weather[0].description
+          : "No disponible"}
+      </p>
+      <p>💨 Viento: {clima.wind.speed} m/s</p>
+    </section>
+  );
+};
+
+export default ListaClima;
